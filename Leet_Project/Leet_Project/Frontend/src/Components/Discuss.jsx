f8092d645d1d0d6975ee87ad423d08bb8066088b
Annshre07 <157482687+Annshre07@users.noreply.github.com>
2025-03-12 16:10:05 +0530

    Delete Leet_Project/Leet_Project directory

--- Leet_Project/Leet_Project/Frontend/src/Components/Discuss.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-function Discuss()
-{
- return(
-<div>Hie you're in a Discussion Page!!</div>
- );
-
-}
-export default Discuss;
--- Leet_Project/Leet_Project/Frontend/src/Components/Post.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-function Post()
-{
- return(
-<div>Hie you're in a Post Page!!</div>
- );
-
-}
-export default Post;
